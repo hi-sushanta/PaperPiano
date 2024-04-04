a0bf95ba170a72de458dee75285623a875b80f5b
hi-sushanta <iamchi@skiff.com>
2024-04-04 19:57:01 +0530

    Add Virtual paper piano with python

---
+++ piano.py
@@ -0,0 +1,73 @@
+# Import necessary libraries
+import pickle  # Pickle library for serializing Python objects
+import cv2  # OpenCV library for computer vision tasks
+import numpy as np  # NumPy library for numerical operations
+from cvzone.HandTrackingModule import HandDetector
+import winsound
+from utils import Utils
+######################################
+cam_id = 1
+width, height = 1280, 720
+map_file_path = "GetCornerPoint/corner.p"
+countries_file_path = "GetPollygonPoint/piano.p"
+######################################
+ 
+file_obj = open(map_file_path, 'rb')
+key_point = pickle.load(file_obj)
+file_obj.close()
+ 
+# Load previously defined Regions of Interest (ROIs) polygons from a file
+if countries_file_path:
+    file_obj = open(countries_file_path, 'rb')
+    polygons = pickle.load(file_obj)
+    file_obj.close()
+else:
+    polygons = []
+ 
+# Open a connection to the webcam
+cap = cv2.VideoCapture(cam_id)  # For Webcam
+# Set the width and height of the webcam frame
+cap.set(3, width)
+cap.set(4, height)
+win_name = "Output Image"
+cv2.namedWindow(win_name, cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty(win_name,cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+
+# Counter to keep track of how many polygons have been created
+counter = 0
+ 
+# Initialize the HandDetector class with the given parameters
+detector = HandDetector(staticMode=False,
+                        maxHands=1,
+                        modelComplexity=1,
+                        detectionCon=0.5,
+                        minTrackCon=0.5)
+ 
+ut = Utils(detector)
+
+while True:
+    # Read a frame from the webcam
+    success, img = cap.read()
+    imgWarped, matrix = ut.warp_image(img, key_point)
+    imgOutput = img.copy()
+ 
+    # Find the hand and its landmarks
+    warped_point = ut.get_finger_location(img, imgWarped,matrix)
+    
+    h, w, _ = imgWarped.shape
+    imgOverlay = np.zeros((h, w, 3), dtype=np.uint8)
+    if warped_point:
+        imgOverlay,gname = ut.create_overlay_image(polygons, warped_point, imgOverlay)
+        imgOutput = ut.inverse_warp_image(img, imgOverlay, key_point)
+        if gname != None:
+            winsound.PlaySound(f"song/{gname}.wav",winsound.SND_ALIAS)
+    
+    cv2.imshow(win_name, imgOutput)
+    
+    key = cv2.waitKey(1)
+
+    if key == 27:
+        break
+  
+cap.release()
+cv2.destroyAllWindows()
---
+++ utils.py
@@ -0,0 +1,138 @@
+import cv2 
+import numpy as np
+import cvzone 
+
+class Utils():
+    def __init__(self,detector):
+        self.detector = detector
+
+    def warp_image(self,img, points, size=[1280, 720]):
+        """
+        Warp the input image based on the provided points to create a top-down view.
+    
+        Parameters:
+        - img: Input image.
+        - points: List of four points representing the region to be warped.
+        - size: Size of the output image.
+    
+        Returns:
+        - imgOutput: Warped image.
+        - matrix: Transformation matrix.
+        """
+        pts1 = np.float32([points[0], points[1], points[2], points[3]])
+        pts2 = np.float32([[0, 0], [size[0], 0], [0, size[1]], [size[0], size[1]]])
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)
+        imgOutput = cv2.warpPerspective(img, matrix, (size[0], size[1]))
+        return imgOutput, matrix
+    
+    def warp_single_point(self,point, matrix):
+        """
+        Warp a single point using the provided perspective transformation matrix.
+    
+        Parameters:
+        - point: Coordinates of the point to be warped.
+        - matrix: Perspective transformation matrix.
+    
+        Returns:
+        - point_warped: Warped coordinates of the point.
+        """
+        # Convert the point to homogeneous coordinates
+        point_homogeneous = np.array([[point[0], point[1], 1]], dtype=np.float32)
+    
+        # Apply the perspective transformation to the point
+        point_homogeneous_transformed = np.dot(matrix, point_homogeneous.T).T
+    
+        # Convert back to non-homogeneous coordinates
+        point_warped = point_homogeneous_transformed[0, :2] / point_homogeneous_transformed[0, 2]
+    
+        return point_warped
+    
+
+    def get_finger_location(self,img, imgWarped,matrix):
+        """
+        Get the location of the index finger tip in the warped image.
+    
+        Parameters:
+        - img: Original image.
+    
+        Returns:
+        - warped_point: Coordinates of the index finger tip in the warped image.
+        """
+        # Find hands in the current frame
+        hands, img = self.detector.findHands(img, draw=False, flipType=True)
+        # Check if any hands are detected
+        if hands:
+            # Information for the first hand detected
+            hand1 = hands[0]  # Get the first hand detected
+            indexFinger = hand1["lmList"][8][0:2]  # List of 21 landmarks for the first hand
+            warped_point = self.warp_single_point(indexFinger, matrix)
+            warped_point = int(warped_point[0]), int(warped_point[1])
+            cv2.circle(imgWarped, warped_point, 5, (255, 0, 0), cv2.FILLED)
+        else:
+            warped_point = None
+    
+        return warped_point
+    
+    def create_overlay_image(self,polygons, warped_point, imgOverlay):
+        """
+        Create an overlay image with marked polygons based on the warped finger location.
+    
+        Parameters:
+        - polygons: List of polygons representing piano keys.
+        - warped_point: Coordinates of the index finger tip in the warped image.
+        - imgOverlay: Overlay image to be marked.
+    
+        Returns:
+        - imgOverlay: Overlay image with marked polygons.
+        """
+        # loop through all the Pianos keys
+        kname = None
+        for polygon, name in polygons:
+            polygon_np = np.array(polygon, np.int32).reshape((-1, 1, 2))
+            result = cv2.pointPolygonTest(polygon_np, warped_point, False)
+            if result >= 0:
+                # winsound.PlaySound(None,0)
+                cv2.polylines(imgOverlay, [np.array(polygon)], isClosed=True, color=(0, 255, 0), thickness=2)
+                cv2.fillPoly(imgOverlay, [np.array(polygon)], (0, 255, 0))
+                cvzone.putTextRect(imgOverlay, name, polygon[0], scale=1, thickness=1)
+                cvzone.putTextRect(imgOverlay, name, (0, 100), scale=8, thickness=5)
+                # winsound.PlaySound(f"song/{name}.wav",winsound.SND_ALIAS)
+                kname = name
+
+        return imgOverlay,kname
+    
+    def inverse_warp_image(self,img, imgOverlay, key_point):
+        """
+        Inverse warp an overlay image onto the original image using provided key points.
+    
+        Parameters:
+        - img: Original image.
+        - imgOverlay: Overlay image to be warped.
+        - map_points: List of four points representing the region on the piano board.
+    
+        Returns:
+        - result: Combined image with the overlay applied.
+        """
+        # Convert map_points to NumPy array
+        key_point = np.array(key_point, dtype=np.float32)
+    
+        # Define the destination points for the overlay image
+        destination_points = np.array([[0, 0], [imgOverlay.shape[1] - 1, 0], [0, imgOverlay.shape[0] - 1],
+                                    [imgOverlay.shape[1] - 1, imgOverlay.shape[0] - 1]], dtype=np.float32)
+    
+        # Calculate the perspective transform matrix
+        M = cv2.getPerspectiveTransform(destination_points, key_point)
+    
+        # Warp the overlay image to fit the perspective of the original image
+        warped_overlay = cv2.warpPerspective(imgOverlay, M, (img.shape[1], img.shape[0]))
+    
+        # Combine the original image with the warped overlay
+        result = cv2.addWeighted(img, 1, warped_overlay, 0.65, 0, warped_overlay)
+    
+        return result
+    
+    
+    
+    
+ 
+
